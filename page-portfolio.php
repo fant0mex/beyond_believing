af37ee8a12fef193c34a67977eecec215798453e
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-09-19 17:59:58 +0100

    Counts through portfolio pieces/ closes rows

--- page-portfolio.php
+++ page-portfolio.php
@@ -37,8 +37,25 @@
         <?php if (have_posts()) : while($the_query->have_posts()) : $the_query->the_post();?>
 
         <div class="col-sm-3 portfolio-piece">
-          <h3><?php the_title(); ?></h3>
+
+          <?php
+            $thumbnail_id = get_post_thumbnail_id();
+            $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'thumnail-size', true);
+          ?>
+
+          <p><a href="<?php the_permalink(); ?>"><img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php the_title(); ?> graphic"></a></p>
+          <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
         </div>
+
+        <?php $portfolio_count = $the_query->current_post + 1; ?>
+
+        <?php if ($portfolio_count % 4 == 0): ?>
+
+        </div>
+
+        <div class="row">
+
+        <?php endif; ?>
 
         <?php endwhile; endif; ?>
 
